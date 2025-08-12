25ba6f77afc2d59d8d0803f114b41997851fd5c4
Sphinzy <chumsatsya2703@gmail.com>
2025-08-12 20:44:57 +0700

    page done

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,11 +39,11 @@ var swiper = new Swiper(".switchSwiper", {
     },
     breakpoints: {
         640: {
-            slidesPerView: 2,
+            slidesPerView: 1,
             spaceBetween: 20,
         },
         768: {
-            slidesPerView: 4,
+            slidesPerView: 2,
             spaceBetween: 40,
         },
         1024: {
